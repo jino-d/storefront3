352f614e9fa558b1154d5efc9c65a16ff945e523
Spectrum One Mac <jcdologmanding@spectrumone.co>
2022-09-28 17:20:51 +0800

    Sending emails section.

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,34 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Jino'}
+        )
+        message.send(['jcdologmanding@spectrumone.co'])
+        # message = EmailMessage(
+        #      'subject',
+        #     'message',
+        #     'jino.c.dologmanding@gmail.com',
+        #     ['jcdologmanding@spectrumone.co'],
+        # )
+        # message.attach_file('playground/static/images/bg.jpeg')
+        # message.send()
+        # mail_admins(
+        #     'subject',
+        #     'message',
+        #     html_message='message'
+        # )
+        # send_mail(
+        #     'subject',
+        #     'message',
+        #     'jino.c.dologmanding@gmail.com',
+        #     ['jcdologmanding@spectrumone.co'],
+        # )
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html', {'name': 'Jino'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -176,4 +176,15 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'jino.c.dologmanding@gmail.com'
 
+ADMINS = [
+    ('Jino', 'jino.c.dologmanding@gmail.com')
+]
+
+# CELERY_BROKER_URL = 'redis://localhost:6379/1'
